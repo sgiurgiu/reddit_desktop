d5f0c56ee3441b2b37ec64f8388a6770e441d462
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-08-25 01:18:34 -0400

    Added ubuntu packaging, changes to how libraries a linked and packages are generated

--- docker/build_reddit_desktop.sh
+++ docker/build_reddit_desktop.sh
@@ -1,11 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir /tmp/build
 cd /tmp/build
 
-cmake -GNinja -DFONTS_DIRECTORY=/usr/share/reddit_desktop -DCPACK_GENERATOR=DEB -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTS=OFF -DENABLE_M4DC=ON -DENABLE_CMARK=OFF /tmp/reddit_desktop
+
+CMAKE_ARGS="-GNinja -DFONTS_DIRECTORY=/usr/share/reddit_desktop -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTS=OFF -DENABLE_M4DC=ON -DENABLE_CMARK=OFF"
+
+if [ -n "$1" ]
+then
+    distro=$1
+    CMAKE_ARGS="${CMAKE_ARGS} -DCPACK_DISTRIBUTION=${distro}"
+    if [[ "${distro}" == "ubuntu" || "${distro}" == "debian" ]]
+    then
+        echo "Building a deb package"
+        CMAKE_ARGS="${CMAKE_ARGS} -DCPACK_GENERATOR=DEB"
+    fi
+    if [[ "${distro}" == "fedora" || "${distro}" == "redhat" || "${distro}" == "centos" || "${distro}" == "suse" ]]
+    then
+        echo "Building an rpm package"
+        CMAKE_ARGS="${CMAKE_ARGS} -DCPACK_GENERATOR=RPM"
+    fi
+fi
+
+cmake ${CMAKE_ARGS} /tmp/reddit_desktop
 
 ninja package
 
-mkdir /tmp/reddit_desktop/packages
+mkdir -p /tmp/reddit_desktop/packages
 cp *.deb /tmp/reddit_desktop/packages/
