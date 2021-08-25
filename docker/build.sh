af779178b19b25b385da7d6f658054b0f9d5512f
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-08-24 21:45:19 -0400

    added icons and lowered boost requirement

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ root=$(git rev-parse --show-toplevel)
 
 podman rm -f rd
 
-buildah bud  -f Dockerfile.debian  -t reddit_desktop_debian:build
+#buildah bud  -f Dockerfile.debian  -t reddit_desktop_debian:build
 
 podman run --name rd --entrypoint=/tmp/workdir/build_reddit_desktop.sh  -v "${root}":/tmp/reddit_desktop/:Z localhost/reddit_desktop_debian:build
 
