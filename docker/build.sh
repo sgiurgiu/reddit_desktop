35e65b5b7752f6f6c0db9b6a9ca9cb7362ab6a23
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-08-31 22:23:52 -0400

    added icons and desktop links

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-
 root=$(git rev-parse --show-toplevel)
 
-podman run --rm --name rd -v "${root}":/tmp/reddit_desktop/:Z registry:5000/reddit_desktop_debian:build
-podman run --rm --name rd -v "${root}":/tmp/reddit_desktop/:Z registry:5000/reddit_desktop_ubuntu:build
-podman run --rm --name rd -v "${root}":/tmp/reddit_desktop/:Z registry:5000/reddit_desktop_fedora:build
+if [ -z $1 ]; then
+    distros=("debian" "ubuntu" "fedora")
+else
+    distros=($1)
+fi
 
+for distro in "${distros[@]}"; do
+    podman run --rm --name rd -v "${root}":/tmp/reddit_desktop/:Z registry:5000/reddit_desktop_$distro:build
+done
 
