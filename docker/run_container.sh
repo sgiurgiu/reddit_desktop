490d8b953b53cdcf7921d2845fe6a49a7997a7d9
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-09-07 23:07:16 -0400

    install xdg-utils and firefox in the runtime container

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -6,4 +6,5 @@ podman run --rm -v ~/.config/reddit_desktop:/root/.config/reddit_desktop \
         -v /tmp/.X11-unix:/tmp/.X11-unix --security-opt=label=type:container_runtime_t \
          -e DISPLAY -v /run/user/$(id -u)/:/run/user/0/   -e XDG_RUNTIME_DIR=/run/user/0 \
         -e PULSE_SERVER=/run/user/0/pulse/native \
+        --ipc host \
         localhost/reddit_desktop_runtime
