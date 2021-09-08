1dd6cbe45404403150b611f30d5a3a8fedfcf0f6
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-09-07 22:44:10 -0400

    Updated the runtime container build file and the way to run it

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,1 +1,9 @@
-podman run --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri --security-opt=label=type:container_runtime_t -e DISPLAY localhost/reddit_desktop_runtime
+#!/bin/bash
+
+mkdir -p ~/.config/reddit_desktop
+
+podman run --rm -v ~/.config/reddit_desktop:/root/.config/reddit_desktop \
+        -v /tmp/.X11-unix:/tmp/.X11-unix --security-opt=label=type:container_runtime_t \
+         -e DISPLAY -v /run/user/$(id -u)/:/run/user/0/   -e XDG_RUNTIME_DIR=/run/user/0 \
+        -e PULSE_SERVER=/run/user/0/pulse/native \
+        localhost/reddit_desktop_runtime
