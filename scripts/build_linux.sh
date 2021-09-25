63539bf72440e88366651edd0e56c233ec3b23ed
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-09-25 14:35:11 -0400

    building and saving a runtime container with the app

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-root=$(git rev-parse --show-toplevel)
+if [ -z ${CI_PROJECT_DIR+x} ]; then
+    root=$(git rev-parse --show-toplevel)
+else
+    root=${CI_PROJECT_DIR}
+fi
 
 if [ -z ${REDDITDESKTOP_VERSION_MAJOR+x} ]; then
     REDDITDESKTOP_VERSION_MAJOR="1"
@@ -26,4 +30,10 @@ for distro in "${distros[@]}"; do
             -e REDDITDESKTOP_VERSION_PATCH="${REDDITDESKTOP_VERSION_PATCH}" \
             registry.zergiu.com:5000/reddit_desktop_$distro:build
 done
+
+podman rmi -f reddit_desktop_runtime:latest
+buildah bud --build-arg RDRPM=/tmp/reddit_desktop/packages/reddit_desktop-${REDDITDESKTOP_VERSION_MAJOR}.${REDDITDESKTOP_VERSION_MINOR}.${REDDITDESKTOP_VERSION_PATCH}-fedora.rpm \
+            -v "${root}":/tmp/reddit_desktop/:Z \
+            -f  ${root}/docker/Dockerfile.fedora.runtime -t reddit_desktop_runtime
+podman save -o ${root}/packages/reddit_desktop_runtime.tar reddit_desktop_runtime:latest
 
