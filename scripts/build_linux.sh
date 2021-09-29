25492027319f9f230f04c89bab915aa7c5c0f466
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-09-28 23:04:01 -0400

    compressing the saved container image

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -37,5 +37,5 @@ podman rmi -f reddit_desktop_runtime:latest || true
 buildah bud --build-arg RDRPM=/tmp/reddit_desktop/packages/reddit_desktop-${REDDITDESKTOP_VERSION_MAJOR}.${REDDITDESKTOP_VERSION_MINOR}.${REDDITDESKTOP_VERSION_PATCH}-fedora.rpm \
             -v "${root}":/tmp/reddit_desktop/:Z \
             -f  ${root}/docker/Dockerfile.fedora.runtime -t reddit_desktop_runtime
-podman save -o ${root}/packages/reddit_desktop_runtime.tar reddit_desktop_runtime:latest
+podman save reddit_desktop_runtime:latest | gzip -9 -n > ${root}/packages/reddit_desktop_runtime.tar.gz
 
