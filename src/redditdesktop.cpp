fdc474c795d82179608083d8e6dc91e91735984c
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2021-10-07 19:18:12 -0400

    ensured that we have at least 2 network threads

--- src/redditdesktop.cpp
+++ src/redditdesktop.cpp
@@ -19,7 +19,7 @@ constexpr auto MEDIA_DOMAINS_POPUP_TITLE = "Media Domains Management";
 
 RedditDesktop::RedditDesktop(boost::asio::io_context& uiContext):
     uiExecutor(uiContext.get_executor()),
-    client("api.reddit.com","oauth.reddit.com",std::thread::hardware_concurrency()),
+    client("api.reddit.com","oauth.reddit.com",std::max(2u, std::thread::hardware_concurrency())),
     loginWindow(&client,uiExecutor),loginTokenRefreshTimer(uiExecutor),
     loggingWindow(std::make_shared<LoggingWindow>(uiExecutor))
 {
