0843d2935f1d16fa94827b112a203d89473755ef
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2024-09-18 00:46:23 -0400

    fixed fmt::join missing include

--- src/connections/redditsrsubscriptionconnection.cpp
+++ src/connections/redditsrsubscriptionconnection.cpp
@@ -1,26 +1,30 @@
 #include "redditsrsubscriptionconnection.h"
 #include <fmt/format.h>
+#include <fmt/xchar.h>
 
-
-RedditSRSubscriptionConnection::RedditSRSubscriptionConnection(const boost::asio::any_io_executor& executor,
-                                                               boost::asio::ssl::context& ssl_context,
-                                                               const std::string& host, const std::string& service,
-                                                               const std::string& userAgent):
-    RedditConnection(executor,ssl_context,host,service),userAgent(userAgent)
+RedditSRSubscriptionConnection::RedditSRSubscriptionConnection(
+    const boost::asio::any_io_executor& executor,
+    boost::asio::ssl::context& ssl_context,
+    const std::string& host,
+    const std::string& service,
+    const std::string& userAgent)
+: RedditConnection(executor, ssl_context, host, service), userAgent(userAgent)
 {
-
 }
-void RedditSRSubscriptionConnection::updateSrSubscription(const std::vector<std::string>& subreddits,
-                          SubscriptionAction action,
-                          const access_token& token)
+void RedditSRSubscriptionConnection::updateSrSubscription(
+    const std::vector<std::string>& subreddits,
+    SubscriptionAction action,
+    const access_token& token)
 {
     this->action = action;
     this->token = token;
-    int split_size = 10; //we found that subscribing to too many subreddits at once can error out
-    for(size_t i = 0; i < subreddits.size(); i += split_size)
+    int split_size = 10; // we found that subscribing to too many subreddits at
+                         // once can error out
+    for (size_t i = 0; i < subreddits.size(); i += split_size)
     {
         auto last = std::min(subreddits.size(), i + split_size);
-        subredditsChunks.emplace_back(subreddits.begin() + i, subreddits.begin() + last);
+        subredditsChunks.emplace_back(subreddits.begin() + i,
+                                      subreddits.begin() + last);
     }
     currentPos = subredditsChunks.cbegin();
     resp.data = true;
@@ -29,7 +33,9 @@ void RedditSRSubscriptionConnection::updateSrSubscription(const std::vector<std:
 
 void RedditSRSubscriptionConnection::subscribeToCurrentChunk()
 {
-    if(action == SubscriptionAction::NotSet || currentPos == subredditsChunks.cend()) return;
+    if (action == SubscriptionAction::NotSet ||
+        currentPos == subredditsChunks.cend())
+        return;
     request_t request;
     request.version(11);
     request.method(boost::beast::http::verb::post);
@@ -38,13 +44,16 @@ void RedditSRSubscriptionConnection::subscribeToCurrentChunk()
     request.set(boost::beast::http::field::accept, "*/*");
     request.set(boost::beast::http::field::connection, "keep-alive");
     request.set(boost::beast::http::field::user_agent, userAgent);
-    request.set(boost::beast::http::field::authorization,fmt::format("Bearer {}",token.token));
-    request.set(boost::beast::http::field::content_type, "application/x-www-form-urlencoded");
+    request.set(boost::beast::http::field::authorization,
+                fmt::format("Bearer {}", token.token));
+    request.set(boost::beast::http::field::content_type,
+                "application/x-www-form-urlencoded");
 
-    auto subredditsList = fmt::format("{}",fmt::join(*currentPos,","));
+    auto subredditsList = fmt::format("{}", fmt::join(*currentPos, ","));
 
-    request.body() = fmt::format("sr={}&action={}",
-                                 subredditsList,(action == SubscriptionAction::Subscribe ? "sub" : "unsub"));
+    request.body() =
+        fmt::format("sr={}&action={}", subredditsList,
+                    (action == SubscriptionAction::Subscribe ? "sub" : "unsub"));
 
     request.prepare_payload();
     responseParser->get().body().clear();
@@ -58,23 +67,23 @@ void RedditSRSubscriptionConnection::responseReceivedComplete()
     auto status = responseParser->get().result_int();
     auto body = responseParser->get().body();
 
-    if(status == 200 && resp.data)
+    if (status == 200 && resp.data)
     {
         resp.body = body;
         resp.data = status == 200;
         resp.status = status;
     }
-    else if(resp.data)
+    else if (resp.data)
     {
         resp.body = body;
         resp.data = false;
         resp.status = status;
     }
 
-    if(currentPos == subredditsChunks.cend())
+    if (currentPos == subredditsChunks.cend())
     {
         action = SubscriptionAction::NotSet;
-        signal({},resp);
+        signal({}, resp);
     }
     else
     {
